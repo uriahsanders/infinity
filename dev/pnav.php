763b0b5206f792c9a806d68e0661d3d39700d1e2
Uriah <uriah@infinity-forum.org>
2013-09-18 20:01:04 -0700

    removed pnav

--- dev/pnav.php
+++
@@ -1,1 +0,0 @@
-<div class="pnav"><a href="/accsummary.php">Summary</a> | <a href="/generalsettings.php">General Settings | <a href="/accsettings.php">Account Settings</a> | <a href="/acclook.php">Look & Layout</a> | <a href="/mail.php">Mail</a> </div>
