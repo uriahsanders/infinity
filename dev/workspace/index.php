19fcd67f091f2f375c9157d11c40f7bcbd489bcc
Uriah <uriah@infinity-forum.org>
2013-10-02 22:14:28 -0700

    updated library paths

--- dev/workspace/index.php
+++ dev/workspace/index.php
@@ -4,7 +4,8 @@
 	<meta charset="UTF-8">
 	<title>Infinity workspace</title>
 	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
-	<script src="../NOHTML/NOHTML.js"></script>
+	<script src="../libs/NOHTML.js"></script>
+	<script src="../libs/Tour.js"></script>
 	<script src="workspace.js"></script>
 </head>
 <body>
