3624cebf115ef58db67ea5cd5cc31f9725d729d2
Uriah <uriah@infinity-forum.org>
2013-10-03 19:33:28 -0700

    Finished element creation and modification. New "tickle" function for modifying dynamically added stuff. Testing and logging has already been started

--- dev/workspace/index.php
+++ dev/workspace/index.php
@@ -3,12 +3,14 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Infinity workspace</title>
-	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
-	<script src="../libs/NOHTML.js"></script>
-	<script src="../libs/Tour.js"></script>
-	<script src="workspace.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+	<!-- <script src="../libs/NOHTML.js"></script>
+	<script src="../libs/Tour.js"></script> -->
 </head>
 <body>
 	
+	<script src="workspace.js"></script>
 </body>
 </html>
--- dev/workspace/workspace.js
+++ dev/workspace/workspace.js
@@ -1,37 +1,90 @@
 //ELEMENTS
-var Element = (function(){
-	var Element = function(){
-		console.log('I am an element');
-	};
-	Element.prototype.hi = function(){
-		console.log('Hi from JUST an element');
+var Element = (function() {
+	"use strict";
+	var Element = function(type, name, creator) {
+		console.log('New element created:');
+		this.name = name;
+		this.creator = creator;
+		this.num = Model['num_' + type + 's'];
+		Model[type+'s'][type + this.num] = {
+			name: name,
+			creator: creator,
+			num: this.num
+		};
 	};
 	return Element;
 })();
-var Document = (function(){ //extends Element
-	var Document = function(){
-		Element.call(this);
-		console.log("I am also a document");
+var Document = (function() { //extends Element
+	"use strict";
+	var Document = function(name, creator) {
+		++Model.num_documents;
+		Element.call(this, 'document', name, creator);
+		console.log("This element is a Document named " + this.name + ", created by " + this.creator);
 	};
 	Document.prototype = Object.create(Element.prototype);
 	Document.prototype.constructor = Document;
-	Document.prototype.hi = function(){
-		console.log('Hi from document');
-	};
 	return Document;
 })();
-//continue for each type of element...
+var Task = (function() {
+	"use strict";
+	var Task = function(name, creator) {
+		++Model.num_tasks;
+		Element.call(this, 'task', name, creator);
+		console.log("This element is a Task named " + this.name + ", created by " + this.creator);
+	};
+	Task.prototype = Object.create(Element.prototype);
+	Task.prototype.constructor = Task;
+	return Task;
+})();
+var Table = (function() {
+	"use strict";
+	var Table = function(name, creator) {
+		++Model.num_tables;
+		Element.call(this, 'table', name, creator);
+		console.log("This element is a Table named " + this.name + ", created by " + this.creator);
+	};
+	Table.prototype = Object.create(Element.prototype);
+	Table.prototype.constructor = Table;
+	return Table;
+})();
+var Note = (function() {
+	"use strict";
+	var Note = function(name, creator) {
+		++Model.num_notes;
+		Element.call(this, 'note', name, creator);
+		console.log("This element is a Note named " + this.name + ", created by " + this.creator);
+	};
+	Note.prototype = Object.create(Element.prototype);
+	Note.prototype.constructor = Note;
+	return Note;
+})();
+var Event = (function() {
+	"use strict";
+	var Event = function(name, creator) {
+		++Model.num_events;
+		Element.call(this, 'event', name, creator);
+		console.log("This element is a Event named " + this.name + ", created by " + this.creator);
+	};
+	Event.prototype = Object.create(Element.prototype);
+	Event.prototype.constructor = Event;
+	return Event;
+})();
 //END
 //FUNCTIONS
-var Workspace = (function(){
+var Workspace = (function() {
+	"use strict";
 	//define functions for use in View
-	var Public, Private = {};
+	var Public = {}, Private = {};
+	Public.init = function() {
+		console.log('The workspace has been initiated.');
+		Controller.init();
+	};
 	Private.ajax = {
 		//standard functions for server communication
 	};
 	//let's make add an object to Public for each major feature
 	Public.boards = { //EX:
-		create: function(){
+		create: function() {
 
 		}
 	};
@@ -39,40 +92,78 @@ var Workspace = (function(){
 })();
 //END
 //MVC
-var Model = (function(){
+var Model = (function() {
+	"use strict";
 	//all data: notify view when stuff changes
 	return {
 		//everything is public
-
+		test: false,
+		//How many of each element we have OPEN
+		num_documents: 0,
+		num_tasks: 0,
+		num_tables: 0,
+		num_notes: 0,
+		num_events: 0,
+		documents: {},
+		tasks: {},
+		tables: {},
+		notes: {},
+		events: {}
 	};
 })();
-var View = (function(){
+var View = (function() {
+	"use strict";
 	//UI: change interface when model changes (calls, but does not define functions)
-	var Public, Private = {};
+	var Public = {}, Private = {};
 	Public.changed = null;
-	Public.notify = function(){
-		switch(Public.changed){
+	Public.notify = function() {
+		switch (Public.changed) {
 			//main view stuff: depending on what changed, do something
+			case 'test':
+				console.log('Testing...it works! Model.test = ' + Model['test']);
+				break;
 		}
 	};
 	return Public;
 })();
-var Controller = (function(){
+var Controller = (function() {
+	"use strict";
 	//click handlers: Modify Model
 	return {
 		//everything is public
-		init: function(){
+		init: function() {
+			console.log("Controller now listening for events!");
+			this.modify('test', true);
 			//start listening for changes
 		},
-		modify: function(what, value){
+		//change a constant Model property
+		modify: function(what, value) {
 			//change data
 			Model[what] = value;
 			//notify view
 			View.changed = what; //tell view our most recent change
 			View.notify(); //we just pushed
+			console.log('-modified: Model.' + what);
+		},
+		//change a dynamically added Model property
+		tickle: function(cat, thing, what, newValue) {
+			Model[cat][thing][what] = newValue;
+			View.changed = cat + '-' + thing + '-' + what;
+			View.notify();
+			console.log('-tickled: Model.' + cat + '.' + thing + '.' + what);
 		}
 	};
 })();
 //END
 //START
-Controller.init();
+(function() {
+	"use strict";
+	Workspace.init();
+	//FOR TESTING IN DEV ONLY:
+	// var elDocument = new Document('task', 'uriah');
+	// var elTask = new Task('task', 'uriah');
+	// var elTable = new Table('table', 'uriah');
+	// var elNote = new Note('note', 'uriah');
+	// var elEvent = new Event('event', 'uriah');
+	// Workspace.init(); //for testing hard coded tests
+})();
