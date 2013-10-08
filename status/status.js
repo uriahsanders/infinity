663ced1bedd60e9e6da3d65ecb75941dbd75c00e
relax <relax@infinity-forum.org>
2013-10-09 00:31:16 +0200

    status files that are done.... also pushing this to new

--- status/status.js
+++ status/status.js
@@ -1,11 +1,18 @@
 //changeStatus, getStatus, pollStatus
-var Status = Status || (function($) {
+var Status = Status || (function($) { //thanks for this, did not know you could do else statements this easy in js ^_^
 	//Just run: Status.init() to do everything
 	var Public = {}, Private = {};
-	Private.url = 'status.php';
+	Private.StatusCodes = [ // the different types, just did this an object so we know what everything means, will be using the keys only here
+			0,//: "Offline", //changed to array, better suited for this
+			1,//: "Online",
+			2,//: "Away",
+			3 //: "Busy"
+		];
+	Private.url = '/libs/status.php'; //changed to the right path
 	Private.idleTime = 0;
 	Private.forced = false; //is their current status voluntary?
 	Public.changeStatus = function(id, forced) {
+		$(".status_icon").attr("src", "/images/status/"+id+".png");
 		$.ajax({
 			url: Private.url,
 			type: 'POST',
@@ -16,39 +23,47 @@ var Status = Status || (function($) {
 		});
 	};
 	Public.getStatus = function() {
-		var status;
-		$.ajax({
+		var status = $.ajax({
 			url: Private.url,
 			type: 'GET',
-			data: 'signal=get-status'
+			data: 'signal=get-status',
+			async: false,
 			success: function(res) {
-				status = res;
+				if (res == "error!")
+				{
+					MsgBox("Error", "There was an error, please reload the page and try again!", 3);
+					return;	
+				}
+				$(".status_icon").attr("src", "/images/status/"+res+".png");
+				return res;
 			}
-		});
+		}).responseText;
 		return status;
 	};
 	//Poll server every minute to see if away or now available
 	Private.pollStatus = function() {
+		++Private.idleTime; //they have been idle for one more minute
 		//if they chose to go away, dont make them available
-		if (Private.idleTime === 0 && Private.forced === true) {
-			Public.changeStatus('Available');
+		if (Private.idleTime == 0 && Private.forced == true) {
+			Public.changeStatus(1);
 			//if they dont have another status already (only make them go away if they are "Available")
-		} else if (Private.idleTime > 4 && Public.getStatus() === 'Available') { //5 minutes
-			Public.changeStatus('Away');
+		} else if (Private.idleTime >= 10 && Public.getStatus() == 1) { //10 minutes
+			Public.changeStatus(2);
 			Private.forced = true; //they didnt choose this :P
 		}
-		++Private.idleTime; //they have been idle for one more minute
 	};
 	Public.init = function() {
 		$(document).ready(function() {
+			Public.getStatus();
 			//reset idle time with any movements
 			$(this).on('mousemove keypress', function() {
 				Private.idleTime = 0;
 			});
 			//change status on element click
-			$(document).on('.some-status-class', 'click', function() {
+			$(document).on('click', '#status_icon label', function() {
 				//you may want to change some of this depending on the DOM
-				Public.changeStatus($(this).attr('id'));
+				var new_status = $(this).children("img").attr('alt');
+				Public.changeStatus(new_status);
 			});
 			//keep polling server every minute
 			window.setInterval(function() {
@@ -58,3 +73,17 @@ var Status = Status || (function($) {
 	};
 	return Public;
 })(jQuery); //include jQuery before this
+
+Status.init();
+$(document).ready(function(e) {
+    $(document).on("click", "#status_icon", function()
+		{
+			var icon = $(this).children("img");
+			var mnu =  $(this).children("span");
+			mnu.slideToggle(300);
+			if (typeof(time) !== "undefined")
+				clearInterval(time);
+			var time = setTimeout(function(){mnu.slideUp(300);}, 60000);
+		}
+	);
+});
--- status/status.php
+++ status/status.php
@@ -1,4 +1,6 @@
 <?php
+	@define("INFINITY", true);
+	include_once("relax.php");
     //when i wrote all this i really had no time so
     //you may want to commit a few changes:
     //add includes, split class into a different file, add `status` column to `members`
@@ -7,18 +9,24 @@
     class Status extends SQL{
         //i thought a class might be neccesary since more functions may eventually be added
         //always good to be prepared
-        public static get_status(){
-            $result = $this->Query("SELECT `status` FROM `members` WHERE `ID` = %d", $_SESSION['ID']);
+        function get_status(){
+            $result = $this->Query("SELECT status,status_time FROM `memberinfo` WHERE `ID` = %d", $_SESSION['ID']); //changed to mamberinfo because we don't want things in members (pwd hash and core member info is there(eg if a vurln if found there or something happens there the user can be deleted or corrupt)
+			//also get status_time
             $row = mysql_fetch_assoc($result);
             return $row['status'];
         }
-        public static change_status($status){
-            $this->Query("UPDATE `members` SET `status` = %s WHERE `ID` = %d", $status, $_SESSION['ID']);
+        function change_status($status){
+            $this->Query("UPDATE `memberinfo` SET `status` = %d, `status_time` = %s WHERE `ID` = %d", $status, date("Y-d-m H:i:s"), $_SESSION['ID']); // update the status_time as well...
         }
     }
+	$Status = new Status;
+	
     if(isset($_GET['signal']) && $_GET['signal'] == 'get-status'){
-        return Status::get_status();
+        echo $Status->get_status();
     }
-    if(isset($_POST['signal']) && $_POST['signal'] == 'change-status'){
-        Status::change_status($_POST['status']);
+	if(isset($_POST['signal']) && $_POST['signal'] == 'change-status' && isset($_POST['status']) && preg_match("/^([0-9]+)$/", $_POST['status'])){ 
+		//check the post status as well and we will be using int's to store
+        $Status->change_status($_POST['status']);
     }
+	
+?>
