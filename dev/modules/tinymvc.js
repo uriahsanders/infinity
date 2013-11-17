dfa56921d34334b809d2ecef10219c6fe6a8a6e1
Uriah <uriah@infinity-forum.org>
2013-11-17 00:02:49 -0800

    example now shows every aspect of framework, added function for modifying multiple values at once, can now use either hyphens or arrays anywhere, made sure booleans dont break the functions that use bool ifs

--- dev/modules/tinymvc.js
+++ dev/modules/tinymvc.js
@@ -1,21 +1,22 @@
 "use strict";
-var propFromArray = function(props, to) {
-	var obj = Model.data;
-	for (var i = 0; i < props.length - 1; ++i) { //minus one so we work with property alone
-		obj = obj[props[i]];
-	}
-	if (to === false) return obj[props[props.length - 1]]; //just return property
-	obj[props[props.length - 1]] = to; //change by reference
-}
-var Model = (function() {
+var propFromArray = propFromArray || function(props, to, usr) {
+		var obj = Model.data;
+		for (var i = 0; i < props.length - 1; ++i) { //minus one so we work with property alone
+			obj = obj[props[i]];
+		}
+		if (to === false && usr === false) return obj[props[props.length - 1]]; //just return property
+		obj[props[props.length - 1]] = to; //change by reference
+	};
+var Model = Model || (function() {
 	return {
 		lastChanged: null,
 		//initialize Model
 		create: function(data) {
 			this.data = data;
 		},
-		get: function(what) { //get something from Model, use of '-' for next obj allowed
-			return propFromArray(what.split('-'), false);
+		get: function(what) { //get something from Model
+			if(typeof what === 'string') what = what.split('-');
+			return propFromArray(what, false, false);
 		},
 		//add an element to the Model wihout calling view
 		add: function(what, value) {
@@ -23,9 +24,15 @@ var Model = (function() {
 		},
 		//change an element in the Model (add and call view if non-existant)
 		modify: function(what, value) {
+			//call what as either an array or string broken with -'s
+			if(typeof what === 'string') what = what.split('-');
 			propFromArray(what, value);
 			Model.lastChanged = what.join('-');
 			View.notify();
+		},
+		cascade: function(obj){
+			//call a bunch of modifies() from a hash, arrays not allowed in this one :(
+			for(var i in obj) this.modify(i.split('-'), obj[i]);
 		},
 		//get the Model obj
 		retrieve: function() {
@@ -33,7 +40,7 @@ var Model = (function() {
 		}
 	};
 })();
-var View = (function() {
+var View = View || (function() {
 	return {
 		create: function(func, max) {
 			this.func = func;
@@ -45,7 +52,7 @@ var View = (function() {
 			var c = Model.lastChanged;
 			//parse last changed to get true value from -'s
 			var value = c.split('-');
-			value = propFromArray(value, false);
+			value = propFromArray(value, false, false);
 			//save the states key to value
 			if (this.states[c]) this.states[c].push(value);
 			else this.states[c] = [value];
@@ -62,26 +69,38 @@ var View = (function() {
 		}
 	};
 })();
-var Controller = (function() {
+var Controller = Controller || (function() {
 	return {
 		create: function(func) {
 			func(Model.data);
 		}
 	};
 })();
-var Router = (function() {
+var Router = Router || (function() {
 	return {
 		//define object with key for path and value for function
-		create: function(paths) {
-			this.paths = paths;
+		create: function(obj) {
+			this.paths = obj.paths;
+			this.type = obj.type;
+			this.defaultURL = obj.url || Model.data.url || window.location;
 		},
 		//change URL and do something after
 		goTo: function(param) {
-			window.history.pushState({state: param}, param, param);
-			if (this.paths && this.paths[param]) {
-				//run user defined function for this path
-				this.paths[param]();
+			if (this.type === 'pushState') {
+				window.history.pushState({
+					state: param
+				}, param, param);
+				if (this.paths && this.paths[param]) {
+					//run user defined function for this path
+					this.paths[param]();
+				}
+			} else {
+				//work with hashes
 			}
+		},
+		//run function for this url
+		run: function() {
+			this.goTo(window.location.substring(this.defaultURL.length));
 		}
 	};
 })();
