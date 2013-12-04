cb1ca2c8029cd7f15cf93928b25cdab4e650e411
Uriah <uriah@infinity-forum.org>
2013-12-04 00:27:20 -0800

    now able to switch between dark and light styles on all pages. few style issues for white.css still, but should be fixed soon. Added some JS to manage changes with localstorage, although may switch to sessionstorage or db storage in the future

--- js/base.js
+++ js/base.js
@@ -1,27 +1,46 @@
-// //page transitions
-// (function() {
-// 	$("body").css("display", "none");
-// 	$("body").fadeIn('slow');
-// 	$("a").click(function(e) {
-// 		if ($(this).attr('href').length > 0) {
-// 			e.preventDefault();
-// 			var ref = this.href;
-// 			$("body").fadeOut('slow', function() {
-// 				window.location = ref;
-// 			});
-// 		}
-// 	});
-// })();
-//modals
-$(document).on('click', '#login', function(){
-				$('#login-modal').modal('show');
-			});
-			$(document).on('click', '#register', function(){
-				$('#register-modal').modal('show');
-			});
-			$(document).on('click', '#recover', function(){
-				$('#recover-modal').modal('show');
-			});
-			$(document).on('click', '#contact', function(){
-				$('#contact-modal').modal('show');
-			});
+$(function() {
+	//page transitions
+	// $("body").css("display", "none");
+	// $("body").fadeIn('slow');
+	// $("a").click(function(e) {
+	// 	if ($(this).attr('href').length > 0) {
+	// 		e.preventDefault();
+	// 		var ref = this.href;
+	// 		$("body").fadeOut('slow', function() {
+	// 			window.location = ref;
+	// 		});
+	// 	}
+	// });
+	//use proper stylesheet and css option
+	if(localStorage.css){
+		$('link[href^="css/"]').attr('href', 'css/'+localStorage.css+'.css');
+		handle_cssModify();
+	}
+	//modals
+	$(document).on('click', '#login', function() {
+		$('#login-modal').modal('show');
+	});
+	$(document).on('click', '#register', function() {
+		$('#register-modal').modal('show');
+	});
+	$(document).on('click', '#recover', function() {
+		$('#recover-modal').modal('show');
+	});
+	$(document).on('click', '#contact', function() {
+		$('#contact-modal').modal('show');
+	});
+	$(document).on('click', '#darkcss', function(){
+		$('link[href="css/white.css"]').attr('href', 'css/dark.css');
+		localStorage.css = 'dark';
+		handle_cssModify();
+	});
+	$(document).on('click', '#whitecss', function(){
+		$('link[href="css/dark.css"]').attr('href', 'css/white.css');
+		localStorage.css = 'white';
+		handle_cssModify();
+	});
+	function handle_cssModify(){
+		$('#orgcss').removeClass(localStorage.css === 'white' ? 'black' : 'white');
+		$('#orgcss').addClass(localStorage.css === 'dark' ? 'black' : 'white');
+	}
+});
