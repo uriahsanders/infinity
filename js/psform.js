22dd03841c1c36f2f14d614b0a83303aa0131579
Uriah <uriah@infinity-forum.org>
2013-12-08 21:02:32 -0800

    Finished persistent forms, i think :P

--- js/psform.js
+++ js/psform.js
@@ -1,27 +1,46 @@
 // Persistent form.js
 var PersistentForm = PersistentForm || (function() {
+	sessionStorage.psCount = sessionStorage.psCount || 0;
 	//set instance data
-	var PersistentForm = function(id, name, data) {
+	var PersistentForm = function(name, data) {
 		console.log("Persistent form created.");
-		this.id = id;
 		this.name = name;
 		this.data = data;
-		++PersistentForm.count;
-		this.num = PersistentForm.count;
+		++sessionStorage.psCount;
+		this.num = sessionStorage.psCount;
+		console.log("Connecting new form to container.");
+		var mark = 'ps-' + this.num;
+		var thiz = this;
+		var classes = 'tab-pane fade'; //first tab is special
+		if (this.num === 1) classes += ' in active';
+		$(function() {
+			$('#ps-tabs').append('<li><a href="#' + mark + '"data-toggle="tab">' + name + '</a></li>');
+			$('#ps-content').append('<div class="' + classes + '"id="' + mark + '">' + data + '</div>');
+			$('#ps-tabs a:first').tab('show') //Select first tab
+			//now save everything in session
+			sessionStorage.psForm = $('#ps-formholder').html();
+		});
 	};
-	PersistentForm.count = 0;
 	//create form container
 	PersistentForm.create = function() { //static
 		console.log("Creating PersistentForm container.");
-		//lets create the form, but dont display it
-		var formHolder = [
-			'<div id="ps-formholder">',
-			'<br />',
-			'<div id="ps-drag"><button type="button" class="close"style="color:red;font-size:3em;">×</button></div><br>',
-			'<ul id="ps-tabs"class="nav nav-tabs"></ul>',
-			'<div id="ps-content"class="tab-content"></div>',
-			'</div>'
-		].join('');
+		var psForm = sessionStorage.psForm || false;
+		if (!psForm) {
+			//lets create the form, but dont display it
+			var formHolder = [
+				'<div id="ps-formholder">',
+				'<br />',
+				'<div id="ps-drag"><button id="ps-close"type="button" class="close"style="color:red;font-size:3em;">×</button></div><br>',
+				'<div id="ps-main">',
+				'<ul id="ps-tabs"class="nav nav-tabs"></ul>',
+				'<div id="ps-content"class="tab-content"></div>',
+				'</div>',
+				'</div>'
+			].join('');
+		} else {
+			$(document.body).prepend('<div id="ps-formholder">' + psForm + '</div>');
+			$('#ps-tabs a:first').tab('show') //Select first tab
+		}
 		//click handling
 		$(function() {
 			$(document.body).prepend(formHolder);
@@ -45,44 +64,24 @@ var PersistentForm = PersistentForm || (function() {
 					});
 				});
 			});
-		});
-	};
-	//add form to formholder
-	PersistentForm.prototype.connect = function() {
-		console.log("Connected new form to container.");
-		var mark = 'ps-'+this.num;
-		var thiz = this;
-		var classes = 'tab-pane fade'; //first tab is special
-		if(this.num === 1) classes += ' in active';
-		$(function(){
-			$('#ps-tabs').append('<li><a href="#'+mark+'"data-toggle="tab">'+thiz.name+'</a></li>');
-			$('#ps-content').append('<div class="'+classes+'"id="'+mark+'">'+thiz.data+'</div>');
-			$('#ps-tabs a:first').tab('show') //Select first tab
-			//now save everything in session
-			sessionStorage.setItem('ps-form', $('#ps-formholder').html());
+			//close the formholder
+			$(document).on('click', '#ps-close', function() {
+				$('#ps-formholder').fadeOut();
+			});
 		});
 	};
 	//common enough for func i guess
-	PersistentForm.show = function() { //static
+	PersistentForm.toggle = function() { //static
 		console.log("Showing PersistentForm container.");
 		$(function() {
-			$('#ps-formholder').fadeIn();
+			$('#ps-formholder').toggle();
 		});
 	};
 	return PersistentForm;
 })();
-//test (temp)
-PersistentForm.create();
-var test = [
-	'<br /><div class="lead">Create New Document</div><input type="text"placeholder="Title"class="form-control" /><br />',
-	'<textarea name="" id="" cols="30" rows="10"class="form-control"placeholder="Body"></textarea>'
-].join('');
-var form = new PersistentForm('testps', 'test', test);
-form.connect();
-var form = new PersistentForm('testps', 'test', 'lorem');
-form.connect();
-var form = new PersistentForm('testps', 'test', 'lorem');
-form.connect();
-var form = new PersistentForm('testps', 'test', 'lorem');
-form.connect();
-PersistentForm.show();
+//example:
+// PersistentForm.create(); //create form holder
+// var form = new PersistentForm('Workspace Document', 'my html content'); //add a new tab, (title, content)
+// var form = new PersistentForm('Something', 'my html content');
+// var form = new PersistentForm('Another', 'my html content');
+// PersistentForm.toggle(); //show the formholder
