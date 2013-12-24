e3ca33654e393ffd15a1ce492f07f319afab8112
Uriah <uriah@infinity-forum.org>
2013-12-23 18:49:51 -0800

    logout now works; changed status function to one that actually exists and int param 0 to logout()

--- new/lounge/logout.php
+++ new/lounge/logout.php
@@ -2,6 +2,6 @@
 define("INFINITY", true);
 include_once("../libs/relax.php");
 include_once("../libs/status.php");
-$Status->change_status("0"); //offline
-$member->logout();
+$Status->changeMyStatus("0"); //offline
+$member->logout(0);
 ?>
