a505ccdf8a17b800ca8c51327ce30ca2aba83194
relax <relax@infinity-forum.org>
2014-01-06 02:23:44 +0100

    hope this works now

--- new/forum.php
+++ new/forum.php
@@ -2,8 +2,8 @@
 define("INFINITY", true); // this is so the includes can't get directly accessed
 define("PAGE", "forum"); // this is what page it is, for the links at the top
 include_once("libs/relax.php"); // use PATH from now on
-$member->check_auth();
 include_once(PATH ."core/top.php");
+$member->check_auth();
 
 
 
@@ -14,7 +14,7 @@ include_once(PATH ."core/top.php");
 
 
 <div id="forum_nav">
-	<div id="forum_nav_1"><a href="#!/">Infinity-forum</a></div><i>---------</i><!--
+	<div id="forum_nav_1"><a href="#">Infinity-forum</a></div><i>---------</i><!--
     --><div id="forum_nav_2"><span></span><span><?php 
 	echo "<ul>";
 	$MyRank = $member->getUserRank(0,"getIndex"); //get the current users rank
@@ -27,11 +27,11 @@ include_once(PATH ."core/top.php");
 		$cats = $forum->Query("SELECT * FROM subcat WHERE parent_ID=%d AND min_rank <= %d AND (visible=1 OR %d=%d) ORDER BY index_ desc;",$row3["ID"],  $MyRank, array_search("Admin", $member->ranks), $MyRank);
 		while ($row = mysql_fetch_array($cats))
 		{
-			echo "<li><a href=\"#!/f=$row[ID]/".$forum->convertName($row["name"])."\">$row[name]</a></li>";
+			echo "<li><a href=\"#f=$row[ID]/".$forum->convertName($row["name"])."\">$row[name]</a></li>";
 			$subcat = $forum->Query("SELECT * FROM subforum WHERE parent_ID = %d AND (visible=1 OR %d=%d) ORDER BY index_ desc;", $row["ID"], $MyRank, array_search("Admin", $member->ranks), $MyRank);
 			while ($row2 = mysql_fetch_array($subcat))
 			{
-				echo "<li><a href=\"#!/f=$row[ID]/".$forum->convertName($row["name"])."&s=$row2[ID]/".$forum->convertName($row2["name"])."\">$row2[name]</a> &#171;</li>";
+				echo "<li><a href=\"#f=$row[ID]/".$forum->convertName($row["name"])."&s=$row2[ID]/".$forum->convertName($row2["name"])."\">$row2[name]</a> &#171;</li>";
 			}
 		}
 		echo "<li>&nbsp;</li>";
